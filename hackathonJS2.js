b3249201b76629a5b83b6e71e4acd00d0a9e496c
yixuanwang <wangyixuan98@hotmail.com>
2015-02-22 09:24:16 -0500

    My first commit

---
+++ hackathonJS2.js
@@ -0,0 +1,59 @@
+// JavaScript Document
+function programComparePreU () {
+    var comparePreU = $("#programPreU").val();
+	
+	if (comparePreU == 1) {
+        console.log ('hi')
+    } 
+	
+	else if (comparePreU == 2) {
+
+    } 
+	
+	else if (comparePreU == 3) {
+
+    }
+	
+	else if (comparePreU == 4) {
+
+    }
+	
+	else if (comparePreU == 5) {
+
+    }
+	
+	else if (comparePreU == 6) {
+
+    }
+	
+	else if (comparePreU == 7) {
+
+    }
+	
+	else if (comparePreU == 8) {
+
+    }
+	
+	else if (comparePreU == 9) {
+
+    }
+	
+	else if (comparePreU == 10) {
+
+    }
+	
+	else if (comparePreU == 11) {
+
+    }
+
+	else if (comparePreU == 12) {
+
+    }
+	else {
+		alert ("asdf its not workinggu")
+	}
+}
+
+
+
+
